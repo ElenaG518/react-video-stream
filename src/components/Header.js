7c616fd7fd13a2f5bb7aa90e630c674860e58c98
Elena Granados <patty518@yahoo.com>
2019-04-19 13:17:41 -0700

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header= () => {
+    return (
+        <div className="ui secondary pointing menu">
+            <div className="right menu">
+            </div>
+        </div>
+    )
+}
+
+export default Header;
