65663a7fff75a77831c8ba042a3234e1c62e1aed
Elena Granados <patty518@yahoo.com>
2019-04-19 13:17:05 -0700

    add header and routes for stream components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+// BrowserRouter  and Link is a react component
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className="ui container">
+            <Header />
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={StreamList} /> 
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit" exact component={StreamEdit} />
+                    <Route path="/streams/delete" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
+                </div>
+            </BrowserRouter>
+        </div>
     );
 }
 
