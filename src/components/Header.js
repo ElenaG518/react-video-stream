0c2b4f2618578bc0aa7c61c2a758ba3edf299fe0
Elena Granados <patty518@yahoo.com>
2019-04-19 13:30:28 -0700

    add links to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 const Header= () => {
     return (
         <div className="ui secondary pointing menu">
+            <Link to="/" className="item">StreaME</Link>
             <div className="right menu">
+            <Link to="/" className="item">All Streams</Link>
             </div>
         </div>
     )
