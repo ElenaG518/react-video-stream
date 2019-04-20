9d9036efb3baa3de688007638bc28e7c1762afa9
Elena Granados <patty518@yahoo.com>
2019-04-20 09:22:01 -0700

    initialize GoogleAuth and clean up index.js

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null};
+
+    componentDidMount() {
+        // When we called Load up here load only allows us to get a signal or a notification of when the loading
+        // process is complete by passing in a callback function.
+        window.gapi.load('client:auth2', () => {
+            // init calls a promise that will notify us once this client library has been succesfully initialized
+            window.gapi.client.init({
+                clientId: '864128091912-b7q3221tbrur7m0pfehrtjjophupqmls.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                 this.auth = window.gapi.auth2.getAuthInstance();
+                 this.setState({isSignedIn: this.auth.isSignedIn.get()})
+             });
+        });
+    }
+
+    renderAuthButton() {
+        if(this.state.isSignedIn === null ) {
+            return <div>I don't know if we are signed in</div>;
+        } else if (this.state.isSignedIn) {
+            return <div>I am signed in</div>
+        } else {
+            return <div>I am not signed in</div>
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>
+        return <div>{this.renderAuthButton()}</div>
     };
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Redux from 'redux';
-// import Redux from 'react-redux';
-// import {createStore, applyMiddlewares} from 'redux-thunk';
 
 import App from './components/App';
 
