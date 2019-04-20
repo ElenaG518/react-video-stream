b9cafe67c1c1d496e05250a10c7c42f60f3ec841
Elena Granados <patty518@yahoo.com>
2019-04-20 14:01:11 -0700

    add types to actions and export action types as variables to avoid making typos that can cause our state to not be updated and therefore our components to not be rerendered

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,19 @@
+import {  SIGN_OUT } from './types';
+
+
 // action creators return objects with a property of type.  Remember that dispatch
 // is looking to get back an object with a property of type, not a function
 
+
+export const SIGN_IN = 'SIGN_IN';
 export const signIn = () => {
     return {
-        type: 'SIGN_IN'
+        type: SIGN_IN
     };
 };
 
 export const signOut = () => {
     return {
-        type: 'SIGN_OUT'
+        type: SIGN_OUT
     };
 };
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,14 +1,15 @@
+import {  SIGN_OUT } from '../actions/types';
+import { SIGN_IN } from '../actions';
 // all capitalized to indicate it is a true constant oject that should never change any of the values inside it
-
 const INITIAL_STATE = {
     isSignedIn: null
 };
 
 export default (state= INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SIGN_IN':
+        case SIGN_IN:
             return {...state, isSignedIn: true };
-        case 'SIGN_OUT':
+        case SIGN_OUT:
             return {...state, isSignedIn: false };
         default:
             return state;
