1345cd03e53472c9a2ab81e75465d505b70e7360
Elena Granados <patty518@yahoo.com>
2019-04-20 12:02:55 -0700

    flesh out GoogleAuth component

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -6,25 +6,69 @@ class GoogleAuth extends React.Component {
     componentDidMount() {
         // When we called Load up here load only allows us to get a signal or a notification of when the loading
         // process is complete by passing in a callback function.
+        // arguments to load, if libraries, then syntax is colon (:) separated list of gapi libraries
         window.gapi.load('client:auth2', () => {
             // init calls a promise that will notify us once this client library has been succesfully initialized
             window.gapi.client.init({
                 clientId: '864128091912-b7q3221tbrur7m0pfehrtjjophupqmls.apps.googleusercontent.com',
                 scope: 'email'
             }).then(() => {
+                //we reference the auth instance object
                  this.auth = window.gapi.auth2.getAuthInstance();
-                 this.setState({isSignedIn: this.auth.isSignedIn.get()})
+                 this.setState({isSignedIn: this.auth.isSignedIn.get()});
+                //  listen in a method that we can pass a callback function to, and the callback function will be invoked
+                // anytime the user's status isSignedIn changes
+                 this.auth.isSignedIn.listen(this.onAuthChange);
              });
         });
     }
+
+    // because this is a callback function I'm going to set it up as an arrow functions that 
+    // its context is bound to my component.  One of the ways to bind this to a callback function
+    // is to use the arrow function syntax
+    onAuthChange = () => {
+        this.setState({isSignedIn: this.auth.isSignedIn.get()})
+    }
+
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    // onSignOut = () => {
+    //     this.auth.signOut();
+    // };
 
     renderAuthButton() {
         if(this.state.isSignedIn === null ) {
             return <div>I don't know if we are signed in</div>;
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in</div>
+            return (
+                <button 
+                    className="ui red google button"
+                    // calling signOut directly from the button
+                    // why not use the syntax below:
+                        // onClick = {() => this.callback(arg)} OR
+                        // onClick = {() => this.callback()} 
+                    // not necessary to include parenthesis if we are not sending args to the
+                    // function.  only necessary when binding this in a callback function and
+                    // sending an argument 
+                    // With JSX you pass a function as the event handler
+                    onClick = { this.auth.signOut}
+                >
+                <i className="google icon" />Sign Out</button>
+                )
         } else {
-            return <div>I am not signed in</div>
+            return (
+                <button
+                    className="ui red google button"
+                    // calling signIn by passing a callback function, onSignIn, to the button, which
+                    // in turn will call SignIn.  Notice no parentheses inside the curly braces. if 
+                    // parentheses were added, onSignIn callback function would be ivoked as soon as the 
+                    // component renders
+                    onClick = {this.onSignIn}
+                >
+                <i className="google icon" />Sign in with Google</button>
+                )
         }
     }
 
