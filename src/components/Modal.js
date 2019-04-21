3111f0626c420484786446c32f941ad592bed94d
Elena Granados <patty518@yahoo.com>
2019-04-21 15:32:43 -0700

    refactor modal to accept title, content and actions as props, and SteamDelete to create Modal component according to the props it send to that component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import createBrowserHistory from '../history';
 
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active "></div>
+        <div
+         onClick ={() => createBrowserHistory.push('/')}
+         className="ui dimmer modals visible active"
+        >
+            <div  onClick={e => e.stopPropagation()} className="ui standard modal visible active ">
+                <div className="header">{props.title}</div>
+                <div className="content">
+                    {props.content}
+                </div>
+                <div className="actions">
+                    {props.actions}
+                </div>
+            </div>
         </div>, 
         document.querySelector('#modal')
     );
--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,10 +2,21 @@ import React from 'react';
 import Modal from '../Modal';
 
 const StreamDelete = () => {
+    const actions = (
+        <div>
+            <button className="ui button negative">Submit</button>
+            <button className="ui button">Cancel</button>
+        </div>
+    );
+
     return (
         <div>
             StreamDelete
-            <Modal />
+            <Modal 
+                title= "Delete Modal" 
+                content="Are you Sure you want to delete this stream?"
+                actions={actions}
+            />
         </div>
     );
 };
