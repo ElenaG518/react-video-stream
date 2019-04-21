f66902a5b85a69a49e1fd5f43b83888b1b9b8065
Elena Granados <patty518@yahoo.com>
2019-04-21 15:44:37 -0700

    change delete button to Link component in order to redirect

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -13,13 +13,14 @@ class StreamList extends React.Component {
         if(stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                        <Link to={`/streams/edit/${stream.id}`}
-                            className="ui button primary">
-                            Edit
-                        </Link>
-                    <button className="ui button negative">
+                    <Link to={`/streams/edit/${stream.id}`}
+                        className="ui button primary">
+                        Edit
+                    </Link>
+                    <Link to={`/stream/delete/${stream.id}`}
+                        className="ui button negative">
                         Delete
-                    </button>
+                    </Link>
                 </div>
             )
         }
