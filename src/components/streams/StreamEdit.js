75d98046f7e2d92f9ff6c59482948e14687510ef
Elena Granados <patty518@yahoo.com>
2019-04-21 12:09:56 -0700

    connect component and call fetchStream action creator to get item info, then render component.  Use ownProps to pass params

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,34 @@
 import React from 'react';
+// connect is from react-redux
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>
-};
+// props are being sent by 'react-router-dom' in App file
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    };
 
-export default StreamEdit;
+    render() {
+        console.log(this.props);
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        return (
+            <div className="item">
+                {this.props.stream.title}
+            </div>
+        );
+    }
+
+}
+
+// So again any time that maps state to prop's is called we actually get two arguments.
+// The first is all the state out of our redux store.
+// The second is the prop's object that shows up inside of our components.
+// So now inside of map state to prop's we have all the information we need.
+const mapStateToProps = (state, ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamEdit);
