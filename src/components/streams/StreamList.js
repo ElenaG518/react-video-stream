8348c4f59e09de7fc0881cedbe8174de03a78901
Elena Granados <patty518@yahoo.com>
2019-04-21 10:53:55 -0700

    add create stream button to link to streams/new path

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions'
+import { Link } from 'react-router-dom';
 
 
 class StreamList extends React.Component {
@@ -42,11 +43,24 @@ class StreamList extends React.Component {
         );
     };
 
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style = {{textAlign: 'right'}}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </ Link>
+                </div>
+            );
+        }
+    }
+
     render() {    
         return (
             <div>
                 <h2>Streams</h2>
                 <div className="ui celled list">{this.renderStreamList()}</div>
+                {this.renderCreate()}
             </div>
         );
     }
@@ -57,7 +71,8 @@ const mapStateToProps = state => {
     // Object.values turns all the values inside an object, into an array 
     return {
         streams: Object.values(state.streams),
-        currentUserId: state.auth.userId 
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn 
     };
 }
 
