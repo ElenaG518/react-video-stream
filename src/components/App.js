bd9d3177963c1386099e4f8fd6823ebdf44bb5e6
Elena Granados <patty518@yahoo.com>
2019-04-21 11:14:23 -0700

    import history object to App file, and change BrowserRouter to a simple Router that will use our own history object to route paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 // BrowserRouter  and Link is a react component
-import {BrowserRouter, Route } from 'react-router-dom';
+import {Router, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
 import Header from './Header';
+import createBrowserHistory from '../history'
 
 const App = () => {
     return (
         <div className="ui container">
             
-            <BrowserRouter>
+            <Router history = {createBrowserHistory}>
                 <div>
                     <Header />
                     <Route path="/" exact component={StreamList} /> 
@@ -21,7 +22,7 @@ const App = () => {
                     <Route path="/streams/delete" exact component={StreamDelete} />
                     <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 }
