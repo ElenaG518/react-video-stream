cfec8c32ea8a26b899a9c54838e5e1df3d97aac6
Elena Granados <patty518@yahoo.com>
2019-04-21 10:47:46 -0700

    add edit and delete buttons to list item for items created by the user taht is currently logged in

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -8,18 +8,34 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderAdmin(stream) {
+        if(stream.userId === this.props.currentUserId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            )
+        }
+    }
+
     renderStreamList() {
         return (
             this.props.streams.map(stream => {
                 return (
                     <div className="item" key={stream.id}>
+                        {this.renderAdmin(stream)}
                         <i className="large middle aligned icon camera" />
                         <div className="content">
                             {stream.title}
                             <div className="description">
                                 {stream.description}
                             </div>                    
-                        </div>                    
+                        </div>                  
                     </div>
                 );                
             })
@@ -39,7 +55,10 @@ class StreamList extends React.Component {
 const mapStateToProps = state => {
     // state.streams is being passed as an object, not an array, so
     // Object.values turns all the values inside an object, into an array 
-    return {streams: Object.values(state.streams) };
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId 
+    };
 }
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
