734dace149c0a986258d75704d0f5af1665560d5
Elena Granados <patty518@yahoo.com>
2019-04-21 17:00:13 -0700

    add Switch to routes in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // BrowserRouter  and Link is a react component
-import {Router, Route } from 'react-router-dom';
+import {Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -12,15 +12,16 @@ import createBrowserHistory from '../history'
 const App = () => {
     return (
         <div className="ui container">
-            
             <Router history = {createBrowserHistory}>
                 <div>
                     <Header />
-                    <Route path="/" exact component={StreamList} /> 
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
+                    <Switch >
+                        <Route path="/" exact component={StreamList} /> 
+                        <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                        <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                        <Route path="/streams/:id" exact component={StreamShow} />
+                    </Switch>   
                 </div>
             </Router>
         </div>
